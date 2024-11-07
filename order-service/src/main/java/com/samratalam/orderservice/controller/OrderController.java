700eb56283f25f09a99c7b97eebada47cdd86462
Samrat Alam <samratalamshanto710@gmail.com>
2024-11-07 15:54:24 +0600

    added circuit breaker in order service

--- order-service/src/main/java/com/samratalam/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/samratalam/orderservice/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.samratalam.orderservice.controller;
 import com.samratalam.orderservice.dto.CommonResponse;
 import com.samratalam.orderservice.dto.OrderRequest;
 import com.samratalam.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -25,7 +26,12 @@ public class OrderController {
     }
 
     @PostMapping("/add-order")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallBackInventoryService")
     public CommonResponse addOrder(@RequestBody OrderRequest orderRequest) {
         return orderService.addOrder(orderRequest);
+    }
+
+    public CommonResponse fallBackInventoryService(@RequestBody OrderRequest orderRequest, RuntimeException runtimeException) {
+        return new CommonResponse(500, false, "Error occurred while adding order. Please try again later.", null);
     }
 }
